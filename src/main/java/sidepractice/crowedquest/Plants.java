47d16b10d2628d2c6e0963ad720bbb380d2cc9ba
DuncantheeDuncan <mthembup69@gmail.com>
2019-09-25 07:53:11 +0200

    added crowed quest

---
+++ src/main/java/sidepractice/crowedquest/Plants.java
@@ -0,0 +1,18 @@
+package sidepractice.crowedquest;
+
+
+class Plant {
+}
+
+class Tree extends Plant {
+}
+
+class Vegetable extends Plant {
+}
+
+public class Plants {
+    public static void main(String[] args) {
+        Plant plant = new Tree();
+        Vegetable vegetable = (Vegetable)plant;
+    }
+}
