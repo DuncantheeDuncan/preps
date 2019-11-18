cba685753e8ef5748d30b137bb6dd40103a4b297
DuncantheeDuncan <mthembup69@gmail.com>
2019-11-18 23:43:05 +0200

    pushing

---
+++ src/main/java/sidepractice/kaplanReview/BlankTry.java
@@ -0,0 +1,17 @@
+package kaplanReview;
+class BlankTry{
+
+	public static void main(String[] args) throws IOException{
+		
+	System.out.println("phumlani");
+
+
+	try{
+		System.out.println("insede.. ");
+	}catch(IOException e ){
+		throw new IOException();
+System.out.println("IOException.. ");
+
+	}
+}
+}
